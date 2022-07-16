a81c61ab0ab382f1034176d1b2dc5fff6f7766ef
Milan Zamazal <pdm@zamazal.org>
2022-07-16 21:25:20 +0200

    Expand commentary in pipewire.el
    This is what would be displayed in MELPA package description.

--- pipewire.el
+++ pipewire.el
@@ -25,11 +25,18 @@
 
 ;;; Commentary:
 ;;
-;; PipeWire user interface based on pipewire-lib.
-;; An interactive buffer can be displayed using `M-x pipewire'.
+;; PipeWire user interface and library.
+;; It currently uses pw-cli and pw-metadata command line utilities to
+;; interact with PipeWire.
+;;
+;; An interactive PipeWire buffer can be displayed using `M-x pipewire'.
+;; There you can view basic PipeWire status and change some settings.
 ;; `pipewire-increase-volume', `pipewire-decrease-volume' and
-;; `pipewire-toggle-muted' functions are also suitable to bind on the
-;; multimedia keys.
+;; `pipewire-toggle-muted' functions can be used also standalone and
+;; are suitable to bind on the multimedia keys.
+;;
+;; The package can be used also non-interactively in Elisp programs.
+;; See pipewire-lib.el source file for available functions.
 
 ;;; Code:
 
